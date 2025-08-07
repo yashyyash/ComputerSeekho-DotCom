51b6c7110cd1637f6acbc216fcffd2684bc28ac1
Jidnyasa1711 <jidnyasapatil333@gmail.com>
2025-08-07 20:14:56 +0530

    added logging

--- java-backend/src/main/java/com/seekho/api/controller/CampusLifeController.java
+++ java-backend/src/main/java/com/seekho/api/controller/CampusLifeController.java
@@ -2,15 +2,20 @@ package com.seekho.api.controller;
 
 import com.seekho.api.dto.CampusLifeDto;
 import com.seekho.api.service.CampusLifeService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/campus-life")
 public class CampusLifeController {
+
+    private static final Logger logger = LogManager.getLogger(CampusLifeController.class);
 
     @Autowired
     private CampusLifeService campusLifeService;
@@ -18,20 +23,25 @@ public class CampusLifeController {
     // Create
     @PostMapping
     public ResponseEntity<CampusLifeDto> createCampusLife(@RequestBody CampusLifeDto campusLifeDto) {
+        logger.info("Creating new CampusLife entry: {}", campusLifeDto);
         CampusLifeDto savedDto = campusLifeService.createCampusLife(campusLifeDto);
+        logger.debug("Saved CampusLife: {}", savedDto);
         return ResponseEntity.ok(savedDto);
     }
 
     // Read all
     @GetMapping
     public ResponseEntity<List<CampusLifeDto>> getAllCampusLife() {
+        logger.info("Fetching all CampusLife entries");
         List<CampusLifeDto> campusLifeList = campusLifeService.getAllCampusLife();
+        logger.debug("Found {} entries", campusLifeList.size());
         return ResponseEntity.ok(campusLifeList);
     }
 
     // Read by ID
     @GetMapping("/{id}")
     public ResponseEntity<CampusLifeDto> getCampusLifeById(@PathVariable Long id) {
+        logger.info("Fetching CampusLife by ID: {}", id);
         CampusLifeDto campusLifeDto = campusLifeService.getCampusLifeById(id);
         return ResponseEntity.ok(campusLifeDto);
     }
@@ -42,13 +52,16 @@ public class CampusLifeController {
             @PathVariable Long id,
             @RequestBody CampusLifeDto campusLifeDto
     ) {
+        logger.info("Updating CampusLife with ID: {}", id);
         CampusLifeDto updatedDto = campusLifeService.updateCampusLife(id, campusLifeDto);
+        logger.debug("Updated CampusLife: {}", updatedDto);
         return ResponseEntity.ok(updatedDto);
     }
 
     // Delete
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCampusLife(@PathVariable Long id) {
+        logger.warn("Deleting CampusLife with ID: {}", id);
         campusLifeService.deleteCampusLife(id);
         return ResponseEntity.ok("CampusLife with ID " + id + " deleted successfully");
     }
