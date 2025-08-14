40268c6e8bce9b7968dabd6070923d1e18a30349
Yash <yashnil532@gmail.com>
2025-08-14 23:59:01 +0530

    deleted wrong folder

--- dotnet-y-backend/DotnetBackend/DTO/CreateStaffRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DotnetBackend.DTO
-{
-    public class CreateStaffRequest
-    {
-        public StaffDTO Staff { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
--- dotnet-y-backend/DotnetBackend/DTO/LoginRequestDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿// File: DTO/LoginRequestDTO.cs
-namespace DotnetBackend.DTO
-{
-    public class LoginRequestDTO
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
